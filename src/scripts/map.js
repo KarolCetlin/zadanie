167a5e3cfc3e877f96fb29adb4eae87d5dce9e10
Karol Cetlin <cetlin.karol@gmail.com>
2019-11-27 16:36:10 +0100

    first project upload

---
+++ src/scripts/map.js
@@ -0,0 +1,32 @@
+const slider = document.getElementById('slider');
+const provinces = document.getElementsByTagName('path');
+
+
+
+
+
+    for (let province of provinces) {
+
+        province.addEventListener('mousemove', function () {
+            this.classList.add('province__active');
+
+        });
+
+
+        province.addEventListener('click', function () {
+            let sliderHide = slider.classList.contains('visible__hide');
+
+            if(sliderHide === true){
+                new Slider2(2000);
+            }
+
+            slider.classList.remove('visible__hide');
+            slider.classList.add('visible_active');
+        });
+
+        province.addEventListener('mouseout', function () {
+            province.classList.remove('province__active');
+        });
+
+    }
+
---
+++ src/scripts/slider.js
@@ -0,0 +1,133 @@
+const circleCollection = [...document.querySelectorAll('.slider__circle-list i')];
+const arrowCollection = [...document.querySelectorAll('.slider__arrowsList i')];
+const imgCollection = [...document.querySelectorAll('.slider__position img')];
+
+const activeElement = 'slider__active-element--display';
+const activeDots = 'slider__active-circle--color';
+
+let slideNumber = 1;
+
+
+
+
+
+
+
+class Slider2 {
+    constructor(time) {
+        this.functionsStack = this.functionsStack.bind(this);
+        this.slideScroll = this.slideScroll.bind(this);
+        this.init();
+
+        this.time = time;
+        this.intervalName = '';
+
+        this.circleHandle();
+        this.arrowHandle();
+
+        this.interval(this.time, 'true');
+
+    }
+
+    init(){
+        this.showSlide(imgCollection, activeElement);
+        this.changeSlide(activeElement, imgCollection);
+        this.changeSlide(activeDots, circleCollection);
+    }
+
+
+    functionsStack() {
+
+        this.increaseValue();
+
+        this.showSlide(imgCollection, activeElement);
+        this.changeSlide(activeElement, imgCollection);
+        this.changeSlide(activeDots, circleCollection);
+
+        console.log(slideNumber);
+    }
+
+
+    increaseValue() {
+        slideNumber++;
+        this.slideScroll();
+    }
+
+    reductionValue() {
+        slideNumber--;
+        this.slideScroll();
+    }
+
+    slideScroll() {
+        if (slideNumber === imgCollection.length) {
+            slideNumber = 0;
+        } else if (slideNumber <= -1){
+            slideNumber = imgCollection.length -1;
+        }
+    }
+
+
+    interval(time, intervalWork) {
+        if (intervalWork === 'false') {
+            clearInterval(this.intervalName);
+        } else if(intervalWork === 'true') {
+            this.intervalName = setInterval(this.functionsStack, this.time);
+            console.log(this.intervalName);
+        }
+    }
+
+    restartInterval() {
+        this.interval(0, 'false');
+        this.interval(this.time, 'true');
+    }
+
+
+    arrowHandle() {
+        arrowCollection.forEach(
+            (arrow) => {
+                arrow.addEventListener('click', () => {
+                        console.log(`w co kliknałem ${arrow.dataset.direction}`);
+
+                        if (arrow.dataset.direction === "back") {
+                            this.reductionValue();
+                            this.init();
+                            this.restartInterval();
+
+                        } else if (arrow.dataset.direction === "next") {
+                            this.increaseValue();
+                            this.init();
+                            this.restartInterval();
+                        }
+                    }
+                )
+            }
+        )
+    }
+
+    circleHandle() {
+        circleCollection.forEach(
+            circle => {
+                circle.addEventListener('click', () => {
+                    slideNumber = circle.dataset.index;
+                    this.init();
+                    this.restartInterval();
+                })
+            }
+        )
+    }
+
+    showSlide(array, showClass) {
+        for (let item of array) {
+            item.classList.contains(showClass) ? item.style.display = 'block' : item.style.display = 'none';
+        }
+    }
+
+    changeSlide(activeClassName, nameCollectionElements) {
+
+        const activeElement = nameCollectionElements.findIndex(title => title.classList.contains(activeClassName));
+        nameCollectionElements[activeElement].classList.remove(activeClassName);
+        nameCollectionElements[slideNumber].classList.add(activeClassName);
+    }
+
+
+}
